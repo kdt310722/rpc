0c3a3bc2f2953790108eb94b8d4b9cf8b1c7f0fd
Diep Dang <kdt310722@gmail.com>
2025-01-24 10:27:35 +0700

    refactor!: rpc message type

--- src/rpc/server.ts
+++ src/rpc/server.ts
@@ -2,7 +2,7 @@ import { Emitter } from '@kdt310722/utils/event'
 import { stringifyJson } from '@kdt310722/utils/json'
 import type { AnyObject } from '@kdt310722/utils/object'
 import type { WebSocket } from 'ws'
-import type { WebSocketMessage } from '../types'
+import type { JsonRpcMessage, WebSocketMessage } from '../types'
 import { RPC_NOTIFY_MESSAGE, RpcMessageHandler, type RpcMessageHandlerOptions, type RpcMethodHandler, createNotifyMessage } from '../utils'
 import { type Client, WebSocketServer, type WebSocketServerOptions } from '../websocket'
 
@@ -13,7 +13,7 @@ export type RpcClientEvents = {
 export interface RpcClient<TMetadata extends AnyObject = AnyObject> extends Omit<Client<TMetadata>, 'send'> {
     events: Emitter<RpcClientEvents>
     notify: (method: string, params?: any) => Promise<void>
-    send: (data: any[] | AnyObject) => Promise<void>
+    send: (data: JsonRpcMessage[] | JsonRpcMessage) => Promise<void>
     sendRaw: (data: WebSocketMessage) => Promise<void>
 }
 
@@ -51,14 +51,14 @@ export class RpcWebSocketServer<TRpcClient extends RpcClient = RpcClient> extend
         return this.server.send(socket, stringifyJson(createNotifyMessage(method, params)), clientId)
     }
 
-    public async send(socket: WebSocket, data: any[] | AnyObject, clientId?: number) {
+    public async send(socket: WebSocket, data: JsonRpcMessage[] | JsonRpcMessage, clientId?: number) {
         return this.server.send(socket, stringifyJson(data), clientId)
     }
 
     protected handleConnection(client: Client<TRpcClient['metadata']>) {
         const notify = (method: string, params?: any) => this.notify(client.socket, method, params, client.id)
         const sendRaw = (data: WebSocketMessage) => client.send(data)
-        const send = (data: any[] | AnyObject) => this.send(client.socket, data, client.id)
+        const send = (data: JsonRpcMessage[] | JsonRpcMessage) => this.send(client.socket, data, client.id)
 
         const events = new Emitter<RpcClientEvents>()
         const rpcClient = { ...client, events, notify, send, sendRaw } as TRpcClient
